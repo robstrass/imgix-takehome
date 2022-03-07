81fd9a55d588299eb26efac3ff7559e9991e4347
Rob Strasser <roestrasser@sbcglobal.net>
2022-03-07 14:49:27 -0800

    removed unneeded files, got page to render

---
+++ src/Components/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './main.modules.css';
+
+export default function Main() {
+    return (
+        <div className={style.mainContainer}>
+            <div>hi</div>
+        </div>
+    )
+}
