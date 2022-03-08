f31794f91a7de78a3cb72d681934200376f238c3
Rob Strasser <roestrasser@sbcglobal.net>
2022-03-07 17:44:43 -0800

    styled form

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -10,7 +10,7 @@ export default function Form({ setParameters }) {
     const [width, setWidth] = useState('');
     const [textColor, setTextColor] = useState('');
     const [textSize, setTextSize] = useState('');
-    
+
     return (
         <>
             <form className={style.mainForm}>
@@ -26,7 +26,58 @@ export default function Form({ setParameters }) {
                         id='imgLink'
                         placeholder='i.e. https://assets.imgix.net/examples/butterfly.jpg'
                         value={imgUrl}
+                        type='url'
                         onChange={e => setImgUrl(e.target.value)}
+                    />
+                </div>
+                <div className={style.mainInputWrapper}>
+                    <label
+                        className={style.mainLabel}
+                        htmlFor='imgBlend'
+                    >
+                        Image Overlay Color: <span className={style.mainSpan}>
+                            <a href='https://htmlcolorcodes.com/color-picker/'>Pick a Hex Code Here!</a>
+                        </span>
+                    </label>
+                    <input
+                        className={style.mainInput}
+                        id='imgBlend'
+                        placeholder='Hex # i.e. FF5733'
+                        value={imgBlend}
+                        type='text'
+                        onChange={e => setImgBlend(e.target.value)}
+                    />
+                </div>
+                <div className={style.mainInputWrapper}>
+                    <label
+                        className={style.mainLabel}
+                        htmlFor='height'
+                    >
+                        Image Height
+                    </label>
+                    <input
+                        className={style.mainInput}
+                        id='height'
+                        placeholder='i.e. 320px'
+                        value={height}
+                        type='number'
+                        onChange={e => setHeight(e.target.value)}
+                    />
+                </div>
+                <div className={style.mainInputWrapper}>
+                    <label
+                        className={style.mainLabel}
+                        htmlFor='width'
+                    >
+                        Image Width
+                    </label>
+                    <input
+                        className={style.mainInput}
+                        id='width'
+                        placeholder='i.e. 640px'
+                        value={width}
+                        type='number'
+                        onChange={e => setWidth(e.target.value)}
                     />
                 </div>
                 <div className={style.mainInputWrapper}>
@@ -41,6 +92,7 @@ export default function Form({ setParameters }) {
                         id='imgText'
                         placeholder='i.e. Checkout my sweet wings!'
                         value={imgText}
+                        type='text'
                         onChange={e => setImgText(e.target.value)}
                     />
                 </div>
@@ -58,6 +110,7 @@ export default function Form({ setParameters }) {
                         id='textColor'
                         placeholder='i.e. FF5733'
                         value={textColor}
+                        type='text'
                         onChange={e => setTextColor(e.target.value)}
                     />
                 </div>
@@ -73,54 +126,8 @@ export default function Form({ setParameters }) {
                         id='textSize'
                         placeholder='i.e. 24px'
                         value={textSize}
+                        type='number'
                         onChange={e => setTextSize(e.target.value)}
-                    />
-                </div>
-                <div className={style.mainInputWrapper}>
-                    <label
-                        className={style.mainLabel}
-                        htmlFor='imgBlend'
-                    >
-                        Image Overlay Color: <span className={style.mainSpan}>
-                            <a href='https://htmlcolorcodes.com/color-picker/'>Pick a Hex Code Here!</a>
-                        </span>
-                    </label>
-                    <input
-                        className={style.mainInput}
-                        id='imgBlend'
-                        placeholder='Hex # i.e. FF5733'
-                        value={imgBlend}
-                        onChange={e => setImgBlend(e.target.value)}
-                    />
-                </div>
-                <div className={style.mainInputWrapper}>
-                    <label
-                        className={style.mainLabel}
-                        htmlFor='height'
-                    >
-                        Image Height
-                    </label>
-                    <input
-                        className={style.mainInput}
-                        id='height'
-                        placeholder='i.e. 320px'
-                        value={height}
-                        onChange={e => setHeight(e.target.value)}
-                    />
-                </div>
-                <div className={style.mainInputWrapper}>
-                    <label
-                        className={style.mainLabel}
-                        htmlFor='width'
-                    >
-                        Image Width
-                    </label>
-                    <input
-                        className={style.mainInput}
-                        id='width'
-                        placeholder='i.e. 640px'
-                        value={width}
-                        onChange={e => setWidth(e.target.value)}
                     />
                 </div>
                 <button
